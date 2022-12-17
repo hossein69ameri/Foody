664c022ca91adca470017c8c80c7a3530b6dcd15
Hossein <hossein.arabameri69@gmail.com>
2022-12-17 12:59:22 +0330

    17 - WebView and Fetch Data in InstructionsFragment.kt

--- app/src/main/java/com/example/nourifoodapp1/ui/activity/detail/viewpager/instruction/InstructionsFragment.kt
+++ app/src/main/java/com/example/nourifoodapp1/ui/activity/detail/viewpager/instruction/InstructionsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import com.example.foody.models.Result
 import com.example.nourifoodapp1.R
 import com.example.nourifoodapp1.databinding.FragmentInstructionsBinding
@@ -17,6 +18,13 @@ private lateinit var binding: FragmentInstructionsBinding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentInstructionsBinding.inflate(layoutInflater)
 
+        //get bundle
+        val args = arguments
+        val bundle : Result? = args?.getParcelable("recipeBundle")
+
+        binding.instructionsWebView.webViewClient = object : WebViewClient(){}
+        val webViewClient : String = bundle!!.sourceUrl
+        binding.instructionsWebView.loadUrl(webViewClient)
 
 
         return binding.root
